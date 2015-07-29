c1dea394d6f6e9610f530e88d35f72909b1535f7
Wanderlei Souza <wanderlei.souza@apontador.com>
2015-07-29 13:45:04 -0300

    war file format

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # spring-boot-demo
+
+para compilar (é necessário instalar o Maven3)
+
+```
+mvn clean package
+``` 
+
+para executar
+
+```java -jar java -jar demo-0.0.1-SNAPSHOT.war```
+
+
--- src/main/java/demo/ServletInitializer.java
+++ src/main/java/demo/ServletInitializer.java
@@ -6,8 +6,7 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
 /**
  * ServletInitializer 
  *    Só é necessário para deploy direto em um webserver (tomcat, jetty etc)
- *    veja as anotações com as demais mudanças necessárias no pom.xml
- *    
+ *
  * @author wanderleisouza
  */
 public class ServletInitializer extends SpringBootServletInitializer {
